63537fd83e7cb5d2442b7df9e4995771a55d1d89
Rachel Nurmi <rnurmi@lazparking.com>
2024-02-11 10:31:03 -0500

    Clean up app.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ import HotelList from "./Trip/Hotels/HotelList";
 function App() {
   return (
     <>
-      {/* <div className="container-fluid">
-        <div className="row flex-nowrap">
-          <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark"> */}
       <Nav />
-      {/* </div>
-        </div>
-      </div> */}
+
       <Routes>
         <Route path="/" element={<Summary />} />
         <Route path="hotels" element={<HotelList />} />
