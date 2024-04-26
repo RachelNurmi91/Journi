a7c5ef8847e195d8f2292e4240498cbdc9d835d4
Rachel Nurmi <rnurmi@lazparking.com>
2024-04-26 17:09:06 -0400

    Delete console logs

--- src/Trip/Hotels/Hotel.js
+++ src/Trip/Hotels/Hotel.js
@@ -104,9 +104,7 @@ function Hotel({ fetchUpdatedTrips, ...props }) {
     tripRequest
       .deleteHotel(id)
       .then(() => {
-        console.log("hi");
         fetchUpdatedTrips().then(() => {
-          console.log(props.activeTrip.hotels);
           if (props.activeTrip.hotels.length > 1) {
             props.navigate("/hotels");
           } else {
